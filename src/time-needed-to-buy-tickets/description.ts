56e2f45d669eee9d2c2634ca494164ee105ce16a
pung <fp94214@gmail.com>
2024-09-29 14:52:05 +0800

    xx

--- src/time-needed-to-buy-tickets/description.ts
+++ src/time-needed-to-buy-tickets/description.ts
@@ -12,20 +12,16 @@ export function timeRequiredToBuy(tickets: number[], k: number): number {
       result++
       tickets[index]--
     }
-
     if (index == k && tickets[index] == 0) {
       return result
     }
-
     // index = (index + 1) % tickets.length
     // ⬇
     if (++index == tickets.length) {
       index = 0
     }
-
-    console.log(index)
   }
 }
 
 console.log(timeRequiredToBuy([2, 3, 2], 2))
-// console.log(timeRequiredToBuy([5, 1, 1, 1], 0))
+console.log(timeRequiredToBuy([5, 1, 1, 1], 0))
